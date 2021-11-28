4cdf27b5ae853665169f68d097ef5e32454206d2
Azat <azat.begenchev@yandex.ru>
2021-11-29 00:53:57 +0500

    first

---
+++ start/js/script.js
@@ -0,0 +1,15 @@
+console.log('hello');//
+console.log('hello');
+console.log('hello');
+console.log("Hello " + 'world');
+console.info('Hello')
+// alert('How you doing');
+
+document.getElementById('out').innerHTML = 'Good!';
+document.getElementById('out').innerHTML = 2021;
+document.getElementById('out').innerHTML = '<b>2021</b>';
+
+// ocument.querySelector('.header').innerHTML = 500;
+// document.querySelector('#one').innerHTML = 2021;
+
+let a = document.querySelector('#one').innerHTML = 500;
