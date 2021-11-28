fea181ed7d08e8836ac920b0c2430141cce1b55a
Azat <azat.begenchev@yandex.ru>
2021-11-28 15:41:53 +0500

    dom

---
+++ dom_js/script/41.js
@@ -0,0 +1,21 @@
+// var div4 = document.getElementById('four4');//1 элемент
+// // var p = div4.getElementsByTagName('p');
+// var div = document.getElementsByTagName('div');// всегда массив
+// var p = document.getElementsByClassName('test')//всегда массив
+
+// console.log(p);
+
+// div[2].style.background = 'yellow';
+//===========================
+// var div = document.querySelector('#four4 p');//CSS selector single first element
+// var div2 = document.querySelectorAll('div p'); // collection div
+// console.log(div2);
+//========================================
+// four4.style.background = 'red'
+
+var div = document.querySelector('div');
+var child = div.childNodes;
+console.log(child);
+
+
+
