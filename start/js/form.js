25ec37866ac6a3b39eaa53e5d527be0859af8e8c
Azat <azat.begenchev@yandex.ru>
2022-01-20 23:59:37 +0500

    form22

--- start/js/form.js
+++ start/js/form.js
@@ -15,6 +15,43 @@ document.querySelector('#btn-1').onclick = function () {
 
 document.querySelector('#btn-2').onclick = function () {
     console.log(document.querySelector('#three').value);
-    let date = document.querySelector('.date');
-    let.innerHTML = document.querySelector('#three').value;
+    document.querySelector('.date').innerHTML = document.querySelector('#three').value;
+}
+
+document.querySelector('#four').oninput = function () { //функция работает при перемещение ползунка
+    console.log(document.querySelector('#four').value);
+    document.querySelector('.range-out').innerHTML = document.querySelector('#four').value
+    
+}
+
+// document.querySelector('#btn-3').onclick = function () { //функция работает при нажатии кнопки
+//     console.log(document.querySelector('#four').value);
+//     let four = document.querySelector('.range-out');
+//     four.innerHTML = document.querySelector('#four').value;
+// }
+
+document.querySelector('#btn-4').onclick = () =>{
+    console.log(document.querySelector('#i2').value);
+    let myCheckBox = document.querySelector('#i2');
+    console.log(myCheckBox.checked);
+    if (myCheckBox.checked) {
+        console.log('Нажат');
+    } else {
+        console.log('Не нажат');
+    }
+}
+
+document.querySelector('#btn-5').onclick = (event) =>{
+    event.preventDefault();
+    let text = document.querySelector('#i3');
+    console.log(text.value);
+    text.value = 'one';
+}
+
+document.querySelector('#btn-5').onclick = (event) =>{
+    event.preventDefault();
+    let form = document.querySelector('form');
+    console.log(form);
+    console.log(form.elements.i3.value);
+    console.log(form.elements.i4.value);
 }
