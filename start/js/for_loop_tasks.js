181d8cdf7808f11e3afd9ea3acf74eb79cf1e92c
Azat <azat.begenchev@yandex.ru>
2022-01-26 00:28:31 +0500

    26

--- start/js/for_loop_tasks.js
+++ start/js/for_loop_tasks.js
@@ -7,10 +7,18 @@ for (let i = 0; i < 3; i++) {
 }
 
 outTwo = document.querySelector('.out-2');
-for (let i = 1; i < 4; i++) {
-
+for (let i = 1; i <= 3; i++) {
+    outTwo.innerHTML += i + '<br>';
     for (let k = 0; k < 3; k++) {
         outTwo.innerHTML += '*_';
     }
-    outTwo.innerHTML += k + '<br>';
+    outTwo.innerHTML += '<br>';
+}
+
+outThree = document.querySelector('.out-3');
+for (let i = 1; i <= 4; i++) {
+    for (let k = 0; k < 3; k++) {
+        outThree.innerHTML += '*_';
+    }
+    outThree.innerHTML += '<br>';
 }
