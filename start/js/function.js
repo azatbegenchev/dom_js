320829f3860bc7ad2ca2d63a5be0e5ca54ca7e55
Azat Begenchev <azat.begenchev@yandex.ru>
2022-01-28 13:32:16 +0500

    funct

---
+++ start/js/function.js
@@ -0,0 +1,44 @@
+let f1 = document.querySelector('.f-1');
+function one() {            //объявление функции
+    console.log('work')
+}
+one(); //вызов функции
+
+f1.onclick = one; //вызов функции
+
+console.log(1 + one());
+console.log(one());
+
+function two() {
+    console.log('work 2000');
+    return 54;
+}
+
+two()
+
+console.log(1 + two());
+
+let a = 8;
+let b = 9;
+
+function multi() {
+    console.log(a * b);
+    return a * b;
+}
+
+let c1 = multi();
+let c2 = 10 * multi();
+console.log(c1, c2);
+
+function multi2(x, y) {
+    return x * y;
+}
+
+function multi3(x = 10, y = 5) {
+    return x * y;
+}
+console.log(multi2(4, 5));
+console.log(multi2(20, 5));
+console.log(multi2(20, a));
+
+console.log(multi3(30));
