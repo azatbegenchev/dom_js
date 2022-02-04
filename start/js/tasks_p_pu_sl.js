9afd6fa949fdfba989bf86cbdf05363f6d8cec7d
Azat Begenchev <azat.begenchev@yandex.ru>
2022-02-04 16:11:18 +0500

    0402

---
+++ start/js/tasks_p_pu_sl.js
@@ -0,0 +1,19 @@
+let d1 = [33, 'best', 66, 'best'];
+let input = document.querySelector('.i-1');
+let div = document.querySelector('.out-1');
+function showArr() {
+    console.log(d1);
+}
+
+
+document.querySelector('.b-1').onclick = function f1() {
+    c = d1.push(input.value);
+    div.innerHTML = d1;
+
+
+}
+
+showArr();
+
+
+
