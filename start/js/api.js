2d8d803bf034521b8c1e8207c39f4332ad318238
Azat Begenchev <azat.begenchev@yandex.ru>
2022-02-08 15:03:31 +0500

    weather

--- start/js/api.js
+++ start/js/api.js
@@ -6,7 +6,8 @@ fetch('http://api.openweathermap.org/data/2.5/weather?id=162183&appid=6942eed4cc
         document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg';
         document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
         // https://openweathermap.org/img/wn/02d@2x.png
-        // document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/ ${data.weather[0]['icon']}@2x.png">`;
+        // `<img src="https://openweathermap.org/img/wn/ ${data.weather[0]['icon']}@2x.png">`
+        document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
     })
     .catch(function () {
         //catch any errors
