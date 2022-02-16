c01db2d5a8e47b6ff74f0779ded3289ae420f29c
Azat Begenchev <azat.begenchev@yandex.ru>
2022-02-16 16:55:22 +0500

    eshop

---
+++ online_shop/js/eshop.js
@@ -0,0 +1,36 @@
+var cart = {}; //моя корзина
+$('document').ready(function () {
+    loadGoods();
+})
+
+function loadGoods() {
+    //Загрузка товаров на страницу
+    $.getJSON('goods.json', function (data) {
+        // console.log(data);
+        var out = '';
+        for (var key in data) {
+            out += '<div class="single-goods">';
+            out += '<h3>' + data[key]['name'] + '</h3>';
+            out += '<p>Цена: ' + data[key]['cost'] + '</p>';
+            out += '<img src="' + data[key].image + '">';
+            out += '<button class="add-to-cart" data-art="' + key + '">Купить</button>'
+            out += '</div>';
+        }
+        $('.goods').html(out);
+        $('button.add-to-cart').on('click', addToCart);
+    });
+
+}
+function addToCart() {
+    //Добавляем в корзину
+    var articul = $(this).attr('data-art');
+    if (cart[articul] != undefined) {
+        cart[articul]++;
+    }
+    else {
+        cart[articul] = 1;
+    }
+    cart[articul] = 1;
+    console.log(cart);
+
+}
