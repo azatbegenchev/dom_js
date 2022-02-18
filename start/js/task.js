8feb5b2b5dae8aa19cc0ae86e7ed0d203c6f8aef
Azat <azat.begenchev@yandex.ru>
2022-02-18 17:24:34 +0500

    jq

--- start/js/task.js
+++ start/js/task.js
@@ -1,4 +1,6 @@
-let out = '';
-out += '<p>Test</p>';
+$('document').ready(function () {
+    var out1 = '';
+    out1 += '<p>Hello World Mother fucker </p>';
+    $('.out').html(out1);
+})
 
-$('p').html(out);
